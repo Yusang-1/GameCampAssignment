f9273cee3fc52e395b79fc5718502d90861bbab0
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-08-19 17:38:50 +0900

    Feat : 스킬 SO

---
+++ GameCampAssignment/Assets/Scripts/Spells/SO/SpellSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Spells", menuName = "Spells/Spell")]
+public class SpellSO : ScriptableObject
+{
+    public enum ElementType
+    {
+        Fire,
+        Eearth,
+        Water
+    }
+
+    public string SpellName;
+    public int SpellRank;
+    public ElementType Element;
+}
