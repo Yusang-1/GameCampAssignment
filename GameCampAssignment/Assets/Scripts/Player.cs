af7519eff687d65e359b48a950275ef22b6d6239
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-08-21 16:42:24 +0900

    Feat : 플레이어 스크립트

---
+++ GameCampAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{   
+    public PlayerData PlayerData;
+
+    private List<SpellSO> spellSOList;
+    [SerializeField] List<SpellData> spellList;
+    [SerializeField] PlayerSO playerSO;
+    private Dictionary<SpellSO, SpellData> spellDataDictionary;
+
+    private int coinCount;
+
+    public void Start()
+    {
+        PlayerData = new PlayerData(this, playerSO);
+        spellSOList = new List<SpellSO>();
+        spellList = new List<SpellData>();
+        spellDataDictionary = new Dictionary<SpellSO, SpellData>();
+    }
+
+    public void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        foreach(SpellData spell in spellList)
+        {
+            spell.CheckCoolTIme();
+        }
+    }
+
+    public void AddSpell(SpellSO spell)
+    {
+        if (spellSOList.Contains(spell)) return;
+
+        spellSOList.Add(spell);
+        
+        List<Projectile> projectileList = new List<Projectile>();
+        Projectile projectile;
+
+        if(spellDataDictionary.ContainsKey(spell))
+        {            
+            spellDataDictionary[spell].ActiveSpell();
+            spellList.Add(spellDataDictionary[spell]);
+        }
+        else
+        {
+            SpellData spellData = new SpellData(spell);
+            spellList.Add(spellData);
+            
+            for (int i = 0; i < spell.PoolingCount; i++)
+            {
+                GameObject temp = Instantiate(spell.Projectile, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+                projectile = temp.GetComponent<Projectile>();
+                projectile.InitData(spellData, transform.position);
+                spellData.GetProjectiles(i, temp);
+                temp.SetActive(false);
+                projectileList.Add(projectile);
+            }
+            spellDataDictionary.Add(spell, spellData);
+        }                   
+    }
+
+    public void RemoveSpell(SpellSO spell)
+    {
+        int index = spellSOList.IndexOf(spell);
+
+        spellSOList.Remove(spell);
+        spellList.RemoveAt(index);                
+    }
+
+    public void ShootProjectile(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        spellList.Clear();        
+    }
+}
---
+++ GameCampAssignment/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerData : IDamagable
+{
+    private int maxHP;
+    private int currentHP;
+    private GameObject prefab;
+    private Player player;
+
+    public int MaxHP => maxHP;
+    public int CurrentHP => currentHP;  
+    public GameObject Prefab => prefab;
+
+    public PlayerData(Player player, PlayerSO playerSO)
+    {
+        maxHP = playerSO.MaxHP;
+        currentHP = maxHP;
+        this.player = player;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (currentHP == 0) return;
+
+        currentHP = Mathf.Clamp(currentHP -= damage, 0, maxHP);
+        Debug.Log(currentHP);
+        if(currentHP == 0)
+        {
+            PlayerDead();
+        }
+    }
+
+    private void PlayerDead()
+    {
+        Debug.Log("게임 오버");
+        UIManger.Instance.TurnGameOverPanel(true);
+        player.GameOver();
+    }
+}
