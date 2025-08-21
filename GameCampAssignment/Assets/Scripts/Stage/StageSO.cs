a6130bdb8c53f8fcb49f1879870d922744c29f5c
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-08-21 16:41:44 +0900

    Chore : 스테이지 SO 추가

---
+++ GameCampAssignment/Assets/Scripts/Stage/StageSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageSO", menuName = "StageSO/Stage")]
+public class StageSO : ScriptableObject
+{
+    public int StageIndex;
+    public EnemySO Enemy;
+    public int EnemyCount;    
+}
