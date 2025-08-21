6d1beab53e32396392d0a5c33f1bc23442a78be2
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-08-21 16:42:07 +0900

    Chore : 플레이어 SO 추가

---
+++ GameCampAssignment/Assets/Scripts/PlayerSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "Player/Player")]
+public class PlayerSO : ScriptableObject
+{
+    public string PlayerName;
+    public int MaxHP;
+}
